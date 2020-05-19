5ae65adb0e6dd145ca0c244935e55f6e74e0f1aa
Kacper <wysockikacper98@gmail.com>
2020-05-19 15:39:53 +0200

    Działająca wersja: Zawartość strony głównej z listą samochodów wyświetlaną z bazy danych.

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { KlientComponent } from './klient/klient.component';
 import { SamochodyComponent } from './samochody/samochody.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SamochodyComponent } from './samochody/samochody.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
