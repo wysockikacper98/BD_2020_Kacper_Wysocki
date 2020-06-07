655e4f6a41dfbb15d960b89029493dd3fd7d1962
Kacper <wysockikacper98@gmail.com>
2020-06-07 12:06:16 +0200

    Rejestracja Klientów działanie od strony graficznej TODO: backend rejestracji Klientów
    Took 1 hour 37 minutes

--- gui/src/app/login/login.component.ts
+++ gui/src/app/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PassKlient} from "../interfaceBazyDanych/pass-klient";
 import {PassPracownik} from "../interfaceBazyDanych/pass-pracownik";
 import {LoginService} from "../services/login.service";
 import {Klienci} from "../interfaceBazyDanych/klienci";
 import {Pracownicy} from "../interfaceBazyDanych/pracownicy";
 import {PracownicyService} from "../services/pracownicy.service";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -20,11 +21,24 @@ export class LoginComponent implements OnInit {
   zalogowanyKlient: number;
   zalogownayPracownik: number;
 
+  ekranLogowania: boolean = true;
+
   klient: boolean = false;
   pracownik: boolean = false;
+  wolnyLogin: boolean = false;
+
+  nowyLoginPole: any;
+  noweHasloPole: any;
+  noweHasloPowtorzPole: any;
+  imiePole: any;
+  nazwiskoPole: any;
+  nrTelPole: any;
+  NipPole: any;
 
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService,
+              private router: Router,) {
+  }
 
   ngOnInit(): void {
     this.getPassyKlient();
@@ -38,7 +52,7 @@ export class LoginComponent implements OnInit {
     this.loginService.getPassyKlient().subscribe(passy => this.passyKlienci = passy);
   }
 
-  getPassyPracownicy(): void{
+  getPassyPracownicy(): void {
     this.loginService.getPassyPracownik().subscribe(passy => this.passyPracownicy = passy);
   }
 
@@ -49,17 +63,20 @@ export class LoginComponent implements OnInit {
           this.klient = true;
           this.pracownik = false;
           this.loginService.zalogowanyJakoKlient(klient.ID_KLIENTA);
+          this.router.navigate(["filter"]).then();
         }
       }
     }
   }
-  inPracownik(login: string, haslo: string): void{
+
+  inPracownik(login: string, haslo: string): void {
     for (let pracownik of this.passyPracownicy) {
       if (pracownik.LOGIN == login) {
         if (pracownik.HASLO == haslo) {
-          this.pracownik =  true;
+          this.pracownik = true;
           this.klient = false;
           this.loginService.zalogowanoJakoPracownik(pracownik.ID_PRACOWNIKA);
+          this.router.navigate(["pracownik-wydanie-samochodu"]).then();
         }
       }
     }
@@ -70,5 +87,28 @@ export class LoginComponent implements OnInit {
     this.inPracownik(login, haslo);
     this.inKlient(login, haslo);
   }
+
+  czyLoginWolny(login: string, haslo: string, hasloPowt: string) {
+    if (haslo == hasloPowt) {
+
+      let znaleziono = false
+      for (let klient of this.passyKlienci) {
+        if (klient.LOGIN == login) {
+          znaleziono = true;
+        }
+      }
+      if (!znaleziono) {
+        this.wolnyLogin = true;
+      }else{
+        this.nowyLoginPole = null;
+      }
+
+
+    }else{
+      this.noweHasloPole = null;
+      this.noweHasloPowtorzPole = null;
+    }
+  }
+
 
 }
